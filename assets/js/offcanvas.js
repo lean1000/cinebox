2f0ea6ada044923c81a65875ab625befe91d4929
Leandro Piai Barreto <piail5409@email.com>
2024-11-21 08:29:01 -0300

    atualizado

--- assets/js/offcanvas.js
+++ assets/js/offcanvas.js
@@ -1,7 +1,9 @@
 function abrirNav(){
     const menu = document.getElementById("offcanvas")
+    menu.style.width = "250px"
 }
 
 function fecharnav(){
-    
+    const menu = document.getElementById("offcanvas")
+    menu.style.width = "0"
 }
